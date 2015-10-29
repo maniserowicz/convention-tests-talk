9c987dcf96cf12e1a2f1b2a086f1575d4c1e9570
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-10-30 00:53:05 +0100

    added test each_property_of_IEntity_is_virtual

---
+++ MyApp.Tests/nhibernate_requirements.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Procent.dotnetconf2015.MyApp.Tests
+{
+    public class nhibernate_requirements
+    {
+        [Fact]
+        public void each_property_of_IEntity_is_virtual()
+        {
+            var entities = ConventionsHelper.types()
+                .Where(x => x.IsAssignableTo<IEntity>())
+            ;
+
+            var entity_properties = entities.SelectMany(x => x.GetProperties())
+                .ToList();
+
+            Assert.NotEmpty(entity_properties);
+
+            var non_virtual_getters = entity_properties
+                .Select(x => x.GetGetMethod())
+                .Where(x => x.IsVirtual == false)
+            ;
+
+            Assert.Empty(non_virtual_getters);
+        }
+    }
+}
