cf22a64bf8c0051ff09b5bf8f2ff1e47d174ac8f
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-10-30 00:33:40 +0100

    added test void each_interface_name_starts_with_capital_I()

---
+++ MyApp.Tests/naming_conventions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Procent.dotnetconf2015.MyApp.Tests
+{
+    public class naming_conventions
+    {
+        [Fact]
+        public void each_interface_name_starts_with_capital_I()
+        {
+            var interfaces = ConventionsHelper.interfaces();
+
+            Assert.NotEmpty(interfaces);
+
+            var interfaces_not_starting_with_I = interfaces
+                .Where(x => x.Name.StartsWith("I") == false);
+
+            Assert.Empty(interfaces_not_starting_with_I);
+        }
+    }
+}
