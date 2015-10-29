96e1f30166f29136bc9d928eb72731773ac9d18b
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-10-29 23:50:12 +0100

    added repository class

---
+++ MyApp/DataAccess/UsersRepozytory.cs
@@ -0,0 +1,10 @@
+﻿namespace Procent.dotnetconf2015.MyApp.DataAccess
+{
+    public class UsersRepozytory
+    {
+        public User GetById(int id)
+        {
+            return new User();
+        }
+    }
+}
