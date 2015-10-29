22373cc33be208642294a4287239da8c3451283e
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-10-30 00:16:02 +0100

    added interface for UsersRepository

--- MyApp/DataAccess/UsersRepozytory.cs
+++ MyApp/DataAccess/UsersRepozytory.cs
@@ -1,6 +1,11 @@
 ﻿namespace Procent.dotnetconf2015.MyApp.DataAccess
 {
-    public class UsersRepozytory
+    public interface UsersRepozytoryInterface
+    {
+        User GetById(int id);
+    }
+
+    public class UsersRepozytory : UsersRepozytoryInterface
     {
         public User GetById(int id)
         {
