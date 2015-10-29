e70afe1639416a462b2df9e2c58fda45d8867e54
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-10-29 23:49:29 +0100

    added User class

---
+++ MyApp/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Procent.dotnetconf2015.MyApp
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
