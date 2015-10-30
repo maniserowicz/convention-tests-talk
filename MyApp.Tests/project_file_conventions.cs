a8a6d247b9ce12369aa80db7a82be1b902d0d841
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-10-30 01:40:42 +0100

    added test sql_scripts_should_be_marked_as_EmbeddedResource

---
+++ MyApp.Tests/project_file_conventions.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using Xunit;
+
+namespace Procent.dotnetconf2015.MyApp.Tests
+{
+    public class project_file_conventions
+    {
+        [Fact]
+        public void sql_scripts_should_be_marked_as_EmbeddedResource()
+        {
+            var projectFiles = ConventionsHelper.project_files()
+                .ToList();
+
+            Assert.NotEmpty(projectFiles);
+
+            foreach (var csproj in projectFiles)
+            {
+                var sqls = from d in XDocument.Load(csproj).Descendants()
+                    let include = d.Attribute("Include")
+                    where include != null
+                    where include.Value.EndsWith(".sql")
+                    select d;
+
+                var sqls_as_non_EmbeddedResource = sqls
+                    .Where(x => x.Name.LocalName != "EmbeddedResource");
+
+                Assert.Empty(sqls_as_non_EmbeddedResource);
+            }
+        }
+    }
+}
