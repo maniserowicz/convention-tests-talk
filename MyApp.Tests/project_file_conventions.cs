f0597faaf3b227aeeb7cfc5efd5424aa3b9cea9e
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-10-30 22:59:22 +0100

    added test cshtml_files_should_be_marked_as_Content()

--- MyApp.Tests/project_file_conventions.cs
+++ MyApp.Tests/project_file_conventions.cs
@@ -7,6 +7,29 @@ namespace Procent.dotnetconf2015.MyApp.Tests
     public class project_file_conventions
     {
         [Fact]
+        public void cshtml_files_should_be_marked_as_Content()
+        {
+            var projectFiles = ConventionsHelper.project_files()
+                .ToList();
+
+            Assert.NotEmpty(projectFiles);
+
+            foreach (var csproj in projectFiles)
+            {
+                var sqls = from d in XDocument.Load(csproj).Descendants()
+                    let include = d.Attribute("Include")
+                    where include != null
+                    where include.Value.EndsWith(".cshtml")
+                    select d;
+
+                var sqls_as_non_EmbeddedResource = sqls
+                    .Where(x => x.Name.LocalName != "Content");
+
+                Assert.Empty(sqls_as_non_EmbeddedResource);
+            }
+        }
+
+        [Fact]
         public void sql_scripts_should_be_marked_as_EmbeddedResource()
         {
             var projectFiles = ConventionsHelper.project_files()
