52bbc2d30a7bdc4bca32611bd9f49df128306aad
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-10-30 22:23:22 +0100

    added UsersCtrl class

---
+++ MyApp.Web/Controllers/UsersCtrl.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Procent.dotnetconf2015.MyApp.Web.Controllers
+{
+    public class UsersCtrl : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
