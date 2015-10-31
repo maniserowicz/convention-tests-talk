beb27ec7e67e3ed6c18a831ffea9f8625ee9c5e9
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-10-31 08:56:11 +0100

    added autofac modules for communication and persistence

---
+++ MyApp/_Infrastructure/Composition/CommunicationModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+
+namespace Procent.dotnetconf2015.MyApp._Infrastructure.Composition
+{
+    public class CommunicationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterType<ExternalServiceCommunication>()
+                .AsImplementedInterfaces();
+        }
+    }
+}
---
+++ MyApp/_Infrastructure/Composition/PersistenceModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+
+namespace Procent.dotnetconf2015.MyApp._Infrastructure.Composition
+{
+    public class PersistenceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .Where(x => x.Namespace.Contains(".DataAccess"))
+                .AsImplementedInterfaces();
+        }
+    }
+}
