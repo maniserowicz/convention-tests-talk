1c83369774235008f5771ca0bc7e6217dc1cd237
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-10-31 08:53:25 +0100

    added test each_service_interface_can_be_resolved_from_container()

---
+++ MyApp.Tests/composition_conventions.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Autofac;
+using Xunit;
+
+namespace Procent.dotnetconf2015.MyApp.Tests
+{
+    public class composition_conventions
+    {
+        [Fact]
+        public void each_service_interface_can_be_resolved_from_container()
+        {
+            var assemblies = ConventionsHelper.assemblies()
+                .ToArray();
+
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyModules(assemblies);
+
+            var container = builder.Build();
+
+            var service_interfaces = ConventionsHelper.interfaces()
+                .Where(x => x != typeof (IEntity))
+                .ToList();
+
+            Assert.NotEmpty(service_interfaces);
+
+            foreach (var serviceInterface in service_interfaces)
+            {
+                var error = Record.Exception(
+                    () => container.Resolve(serviceInterface)
+                );
+
+                Assert.Null(error);
+            }
+        }
+    }
+}
